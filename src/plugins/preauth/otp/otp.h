820ee1741190854dbe832ae855097b538d001107
Linus Nordberg <linus@nordu.net>
2011-05-09 17:11:19 +0200

    Clean up a comment.

--- src/plugins/preauth/otp/otp.h
+++ src/plugins/preauth/otp/otp.h
@@ -28,7 +28,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-//#include <stddef.h>
+
 #include <krb5/krb5.h>
 #include <kdb.h>
 #include "adm_proto.h"          /* for krb5_klog_syslog */
